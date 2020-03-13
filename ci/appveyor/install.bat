0c80abea46786954b42590bd099a1384275cf862
Vladislav Yaroshchuk <yaroshchuk2000@gmail.com>
2020-03-13 23:42:55 +0300

    Setup appveyor ci. Deploy not tested yet

--- ci/appveyor/install.bat
+++
@@ -1,2 +0,0 @@
-del "c:\Program Files (x86)\MSBuild\14.0\Microsoft.Common.targets\ImportAfter\Xamarin.Common.targets"
-del "c:\Program Files (x86)\MSBuild\4.0\Microsoft.Common.targets\ImportAfter\Xamarin.Common.targets"
