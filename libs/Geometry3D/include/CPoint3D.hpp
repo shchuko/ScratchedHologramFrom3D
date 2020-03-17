1a22e6349fa04b54f8b7663e479649d08ea6fee3
Tulesha <tulesha_xochet_pit@mail.ru>
2020-03-17 18:36:05 +0300

    Add new documentation to CPoint3D.hpp

--- libs/Geometry3D/include/CPoint3D.hpp
+++ libs/Geometry3D/include/CPoint3D.hpp
@@ -3,8 +3,7 @@
 namespace Geometry3D {
 
     /**
-     * @author Danil Nasibullin 2020
-     * email:
+     * @author Tulesha 2020
      */
     class CPoint3D {
     private:
@@ -13,24 +12,47 @@ namespace Geometry3D {
         double z = 0.0;
 
     public:
-
+        /**
+         * Base constructor
+         */
         CPoint3D() = default;
 
+        /**
+         * Copy constructor Lvalue
+         * @param cPoint3D Point in 3D
+         */
         CPoint3D(const CPoint3D& cPoint3D) noexcept;
 
+        /**
+         * Copy constructor Rvalue
+         * @param cPoint3D Point int 3D
+         */
         CPoint3D(CPoint3D&& cPoint3D) noexcept;
 
+        /**
+         * Constructor with parameters of coordinates
+         * @param x Coordinate X
+         * @param y Coordinate Y
+         * @param z Coordinate Z
+         */
         CPoint3D(double x, double y, double z) noexcept;
 
         /**
          * Get x coordinate
-         * @return x coordinate
+         * @return Coordinate X
          */
         double getX() const noexcept;
 
+        /**
+         * Get y coordinate
+         * @return Coordinate Y
+         */
         double getY() const noexcept;
 
+        /**
+         * Get z coordinate
+         * @return Coordinate Z
+         */
         double getZ() const noexcept;
-
     };
 }
