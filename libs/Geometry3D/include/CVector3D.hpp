f14232876026e6336317d61dfc513bd13e8bfe57
Tulesha <tulesha_xochet_pit@mail.ru>
2020-03-17 18:45:11 +0300

    Add functions and constructors to CVector3D.hpp and CVector3D.cpp

--- libs/Geometry3D/include/CVector3D.hpp
+++ libs/Geometry3D/include/CVector3D.hpp
@@ -4,6 +4,60 @@
 namespace Geometry3D {
 
     class CVector3D {
+    private:
+        double x = 0.0;
+        double y = 0.0;
+        double z = 0.0;
+    public:
+        /**
+         * Base constructor
+         */
+        CVector3D() = default;
+
+        /**
+         * Copy constructor Lvalue
+         * @param cVector3D Vector in 3D
+         */
+        CVector3D(const CVector3D& cVector3D) noexcept;
+
+        /**
+         * Copy constructor Rvalue
+         * @param cVector3D Vector in 3D
+         */
+        CVector3D(CVector3D&& cVector3D) noexcept;
+
+        /**
+         * Constructor with parameters of coordinates
+         * @param x Coordinate X
+         * @param y Coordinate Y
+         * @param z Coordinate Z
+         */
+        CVector3D(double x, double y, double z);
+
+        /**
+         * Get x coordinate
+         * @return Coordinate X
+         */
+        double getX() const noexcept;
+
+        /**
+         * Get y coordinate
+         * @return Coordinate Y
+         */
+        double getY() const noexcept;
+
+        /**
+         * Get z coordinate
+         * @return Coordinte Z
+         */
+        double getZ() const noexcept;
+
+        /**
+         * Get length of 3D vector
+         * @return length of 3D vector
+         */
+        double getLen() const noexcept;
+
 
     };
 
--- libs/Geometry3D/source/CVector3D.cpp
+++ libs/Geometry3D/source/CVector3D.cpp
@@ -3,4 +3,31 @@
 
 namespace Geometry3D {
 
+    CVector3D::CVector3D(const CVector3D &cVector3D) noexcept {
+
+    }
+
+    CVector3D::CVector3D(CVector3D &&cVector3D) noexcept {
+
+    }
+
+    CVector3D::CVector3D(double x, double y, double z) {
+
+    }
+
+    double CVector3D::getX() const noexcept {
+        return 0;
+    }
+
+    double CVector3D::getY() const noexcept {
+        return 0;
+    }
+
+    double CVector3D::getZ() const noexcept {
+        return 0;
+    }
+
+    double CVector3D::getLen() const noexcept {
+        return 0;
+    }
 }
