bb7aea8b64c10fb2e81aa5dfc57667f8937fcfc3
Nikita <novgorodcevnp@gmai.com>
2020-03-20 16:29:24 +0300

    add class COption

--- libs/CliTools/include/COption.hpp
+++ libs/CliTools/include/COption.hpp
@@ -4,6 +4,33 @@ namespace CliTools {
 
     class COption {
 
+        // Fields
+    private:
+
+        std::string shortName;
+        std::string longName;
+        std::string description;
+        std::string argument;
+        bool isRequiredArgument = false;
+
+        // Methods
+    public:
+
+        std::string getShortName();
+        std::string getLongName();
+        std::string getDescription();
+        std::string getOptionData();
+
+        bool isRequired();
+        bool isHasArgument();
+
+    private:
+
+        // Private constructor
+        // Building in COptionBuilder
+        COption()= default;
+
+
     };
 
 }
--- libs/CliTools/source/COption.cpp
+++ libs/CliTools/source/COption.cpp
@@ -1,2 +1,26 @@
+#include <string>
 #include "COption.hpp"
 
+std::string CliTools::COption::getShortName() {
+    return shortName;
+}
+
+std::string CliTools::COption::getLongName() {
+    return longName;
+}
+
+std::string CliTools::COption::getDescription() {
+    return description;
+}
+
+bool CliTools::COption::isHasArgument() {
+    return !argument.empty();
+}
+
+std::string CliTools::COption::getOptionData() {
+    return argument;
+}
+
+bool CliTools::COption::isRequired() {
+    return isRequiredArgument;
+}
