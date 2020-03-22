f03cb8bfc5d5ce71742ab062409f4e016e0947a9
Nikita <novgorodcevnp@gmai.com>
2020-03-22 18:13:50 +0300

    Nested in the CliTools namespace

--- libs/CliTools/source/EOptionBuildError.cpp
+++ libs/CliTools/source/EOptionBuildError.cpp
@@ -1,9 +1,9 @@
 #include "EOptionBuildError.hpp"
 
-EOptionBuildError::EOptionBuildError(std::string &error) {
+CliTools::EOptionBuildError::EOptionBuildError(std::string &error) {
     m_error = error;
 }
 
-const char *EOptionBuildError::what() const noexcept {
+const char *CliTools::EOptionBuildError::what() const noexcept {
     return m_error.c_str();
 }
