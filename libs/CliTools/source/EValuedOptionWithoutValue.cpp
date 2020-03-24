68523f6daf1ebb8e9bea4f5611802b362aa92216
paNoNi <novgorodcevnp@gmail.com>
2020-03-24 18:36:58 +0300

    Add class EValuedOptionWithoutValue exception

---
+++ libs/CliTools/source/EValuedOptionWithoutValue.cpp
@@ -0,0 +1,14 @@
+#include <EValuedOptionWithoutValue.hpp>
+
+namespace CliTools {
+    namespace Exceptions {
+
+        Exceptions::EValuedOptionWithoutValue::EValuedOptionWithoutValue() = default;
+
+        EValuedOptionWithoutValue::EValuedOptionWithoutValue(std::string &error) : EParserError(error) {}
+
+        const char *EValuedOptionWithoutValue::what() const noexcept {
+            return EParserError::what();
+        }
+    }
+}
