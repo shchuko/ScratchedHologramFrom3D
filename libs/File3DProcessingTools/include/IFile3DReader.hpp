75e39832a2f11d2099c4e207c8017f87c0b389e4
Vladislav Yaroshchuk <yaroshchuk2000@gmail.com>
2020-03-29 16:30:40 +0300

    Extend File3DProcessingTools::IFile3DReader class interface

--- libs/File3DProcessingTools/include/IFile3DReader.hpp
+++ libs/File3DProcessingTools/include/IFile3DReader.hpp
@@ -1,4 +1,10 @@
+/**
+ * @author Vladislav Yaroshchuk, github.com/shchuko
+ */
 #pragma once
+
+
+#include "CObject3DData.hpp"
 
 
 namespace File3DProcessingTools {
@@ -6,6 +12,12 @@ namespace File3DProcessingTools {
      * @author Vladislav Yaroshchuk, github.com/shchuko
      */
     class IFile3DReader {
-
+        /**
+         * Read file into CObject3DData
+         * @param filepath Path to file
+         * @return CObject3DData object
+         * @throw File3DProcessingTools::EFile3DReadError if file can't be read
+         */
+        virtual CObject3DData readFile(std::string filepath) = 0;
     };
 }
