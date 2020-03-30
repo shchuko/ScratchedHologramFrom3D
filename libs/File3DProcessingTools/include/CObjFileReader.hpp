396d127309f30d1d8c61c34065cfef38f5a37a46
Vladislav Yaroshchuk <yaroshchuk2000@gmail.com>
2020-03-30 16:27:29 +0300

    Implement CObjFileReader. Change CXX_STANDARD 11 -> 17

--- libs/File3DProcessingTools/include/CObjFileReader.hpp
+++ libs/File3DProcessingTools/include/CObjFileReader.hpp
@@ -1,12 +1,38 @@
 #pragma once
 
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include "EFile3DReadError.hpp"
 #include "IFile3DReader.hpp"
+
 
 namespace File3DProcessingTools {
     /**
      * @author Vladislav Yaroshchuk, github.com/shchuko
      */
     class CObjFileReader : public IFile3DReader {
+    public:
+        /**
+         * Read file into CObject3DData
+         * @param filepath Path to file
+         * @param output_data CObject3DData object to read into
+         * @throw File3DProcessingTools::EFile3DReadError if file can't be read
+         */
+        void readFile(const std::string &filepath, CObject3DData &output_data) override;
 
+    private:
+        std::ifstream fin;
+        CObject3DData *p_data = nullptr;
+        std::string next_line;
+
+        bool parseNextLine() noexcept;
+
+        void parseVertexData() noexcept;
+
+        void parseNormalVectorData() noexcept;
+
+        bool parsePolygonData() noexcept;
     };
 }
