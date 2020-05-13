970b30db4a673bdcaf0bdd0775fc9d9224fdd465
Vladislav Yaroshchuk <yaroshchuk2000@gmail.com>
2020-05-13 15:49:37 +0300

    [issue #72] Add File2dProcessingTools::CPath documentation

--- libs/File2DProcessingTools/include/CPath.hpp
+++ libs/File2DProcessingTools/include/CPath.hpp
@@ -12,27 +12,90 @@ namespace File2DProcessingTools {
         bool _smooth;
         bool _isCycled;
         std::vector<Geometry2D::CPoint2D> CPoints2D;
+
     public:
-        CPath() noexcept; // Default width 1px, default color BLACK, smooth=false
+        /**
+         * Default constructor. Width 1px, default color BLACK, smooth=false
+         */
+        CPath() noexcept;
+
+        /**
+         * Create and setup CPath object
+         * @param width_px Width in px
+         * @param color Color
+         * @param smooth Smooth flag
+         */
         CPath(unsigned int width_px, CVectorGraphicsData::CColor_T &color, bool smooth) noexcept;
-        void setColor(CVectorGraphicsData::CColor_T &color)  noexcept; // цвет
+
+        /**
+         * Set new color
+         * @param color New color
+         */
+        void setColor(CVectorGraphicsData::CColor_T &color) noexcept;
+
+        /**
+         * Get color of the CPath object
+         * @return Color of the CPath object
+         */
         CVectorGraphicsData::CColor_T getColor() const noexcept;
-        void setWidth(unsigned int width_px) noexcept; // толщина в пикселях
+
+        /**
+         * Set width in px
+         * @param width_px Width in px
+         */
+        void setWidth(unsigned int width_px) noexcept;
+
+        /**
+         * Get width of the CPath object
+         * @return Width of the CPath object in px
+         */
         unsigned int getWidth() const noexcept;
-        void setSmooth(bool flag) noexcept; // сглаживание, атрибут для обработки в дальнейшем
+
+        /**
+         * Set smoothing flag
+         * @param flag True to enable smoothing, false to disable
+         */
+        void setSmooth(bool flag) noexcept;
+
+        /**
+         * Get smoothing flag
+         * @return Smoothing flag value
+         */
         bool isSmooth() const noexcept;
 
-        // Добавляет следующую точку к линии
-        // Если линия имеет одну из координат NaN - разрыв в линии, распознается методом toSeparatedPaths()
-        void appendPoint(const Geometry2D::CPoint2D& point) noexcept;
-        void appendPath(CPath &cPath) noexcept;
-        const std::vector<Geometry2D::CPoint2D>& getPoints() const noexcept;
+        /**
+         * Add next CPath point. To add split point, pass {NaN, NaN}
+         * @param point Next CPath point
+         */
+        void appendPoint(const Geometry2D::CPoint2D &point) noexcept;
 
-        // Разбивает линию на несколько линий, разрыв определяется точкой с NaN-координатой или циклом
+        /**
+         * Concatenate CPath object
+         * @param cPath CPath object to contatenate with
+         */
+        void appendPath(CPath &cPath) noexcept;
+
+        /**
+         * Get stored points
+         * @return Stored points
+         */
+        const std::vector<Geometry2D::CPoint2D> &getPoints() const noexcept;
+
+        /**
+         * Split current path to paths (split criterion - {NaN, NaN} point)
+         * @return
+         */
         std::vector<CPath> toSeparatedPaths();
 
-        // Проверяет, зациклена ли кривая. Если да - отмечает, что есть цикл, чтоб не дублировать точку начала/конца при записи в файл
+        /**
+         * Update internal is-current-path-cycled flag (ending == beginning)
+         */
         void updateCycled() noexcept;
+
+        /**
+         * Get is-current-path-cycled flag (ending == beginning)
+         * @return True if current path is cycled
+         */
         bool isCycled() const noexcept;
 
     };
